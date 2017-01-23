a4b273526e4aa03ec0cdfa4221a1047ba667f506
jjlee9 <jjlee3@hotmail.com>
2017-01-23 18:02:03 +0800

    remove unneeded files

--- examples/apps/mstc/SocketApp/impl/Log.cpp
+++
@@ -1,5 +0,0 @@
-#include "detail/pch.h"
-#include "detail/StdOutput.h"
-#include "detail/Log.h"
-
-mstc::base::Monitor<std::ostream&>& g_log{g_out};
--- examples/apps/mstc/SocketApp/impl/StdOutput.cpp
+++
@@ -1,5 +0,0 @@
-#include "detail/pch.h"
-#include "detail/StdOutput.h"
-#include <iostream>
-
-mstc::base::Monitor<std::ostream&> g_out{std::cout};
